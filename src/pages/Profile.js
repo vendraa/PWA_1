24fbfdf8147718b76371c0ab19e00ee7516e4d19
Novendra Anugrah <novendraanugrah@gmail.com>
2024-10-24 14:16:06 +0700

    first commit

---
+++ src/pages/Profile.js
@@ -0,0 +1,8 @@
+export default function Profile() {
+
+    return (
+        <div style={{ display: "flex", justifyContent: "center" }}>
+            <h1 style={{ color: "white" }}>Profile</h1>
+        </div>
+    );
+}
